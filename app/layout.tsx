83de6818bb1c35dbff9bb4ad50760d052eabde26
BEOKS <lee01042000@gmail.com>
2024-01-16 22:18:26 +0900

    feat : chapter 4

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,14 @@
+import '@/app/ui/global.css'
+import {inter} from "@/app/ui/fonts";
+
+/**
+ * This is called a root layout and is required.
+ * Any UI you add to the root layout will be shared across all pages in your application.
+ * You can use the root layout to modify your <html> and <body> tags,
+ * and add metadata (you'll learn more about metadata in a later chapter).
+ * @param children
+ * @constructor
+ */
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
